cd5611e4770495a2cfef1ad18bec0b07e82e84f1
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-07-20 09:48:29 -0400

    wip: routing included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,13 @@ import { ButtonComponent } from './components/button/button.component';
 import { StudyItemsComponent } from './components/study-items/study-items.component';
 import { StudyItemComponent } from './components/study-item/study-item.component';
 import { AddStudyItemComponent } from './components/add-study-item/add-study-item.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: StudyItemsComponent },
+  {path: 'about', component: AboutComponent },
+]
 
 @NgModule({
   declarations: [
@@ -20,13 +27,16 @@ import { AddStudyItemComponent } from './components/add-study-item/add-study-ite
     StudyItemsComponent,
     StudyItemComponent,
     AddStudyItemComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddStudyItem: boolean = true
   subscription!: Subscription
 
-  constructor(private uiService:UiService) { 
+  constructor(private uiService:UiService, private router:Router) { 
     this.subscription = this.uiService.onToggle().subscribe(Value => (this.showAddStudyItem = Value))
   }
 
@@ -21,5 +21,8 @@ export class HeaderComponent implements OnInit {
   }
   toggleStudyItem() {
     this.uiService.toggleAddStudyItem();
+  }
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
